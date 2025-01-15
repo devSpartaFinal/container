d6b6340ab5564f4a7362872022f62de42f9109a3
suyeon <stgood@kookmin.ac.kr>
2025-01-15 20:19:17 +0900

    feat/aws-deploy-2: aws 배포 준비

--- backend/coding_helper/settings.py
+++ backend/coding_helper/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = DRF_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["13.125.174.11"]
+ALLOWED_HOSTS = ["43.201.23.73"]
 
 # React 프론트 관련 / # 모든 출처 허용
 CORS_ALLOW_ALL_ORIGINS = True  
