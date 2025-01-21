4c7e64df1e86b253dbcf938cbf08dfbb062b461b
suyeon <stgood@kookmin.ac.kr>
2025-01-21 11:26:26 +0900

    staging-13: ALL To HTTP

--- backend/coding_helper/settings.py
+++ backend/coding_helper/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = DRF_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["44.222.253.186"]
 
 
 # React 프론트 관련 / # 모든 출처 허용
